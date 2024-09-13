d61a793c355695a81c1fe52baf2026c1a43904c4
Samudrala Venkata Pavan Tarun Kumar <116933974+klu-2200031955@users.noreply.github.com>
2024-09-13 10:29:22 +0530

    Update StudentCourseMapping.js

--- models/StudentCourseMapping.js
+++ models/StudentCourseMapping.js
@@ -1,15 +1,17 @@
 const mongoose = require("mongoose")
 
-const studentcoursemappingschema = mongoose.Schema({
+const studentcoursemappingschema = new mongoose.Schema({
     smapid:{
-        type:String,
-        required:true,
-        unique:true,
-        default: ()=> generateRandomId()
+        type: String,
+        required: true,
+        unique: true,
+        default: () => generateRandomId()
     },
     ccode: {
         type: String,
         required: true,
+        // Ensure it's not marked as unique
+        unique: false
     },
     facultyid: {
        type: String,
@@ -27,12 +29,12 @@ const studentcoursemappingschema = mongoose.Schema({
       type: String,
       required: true
     }
-})
+});
 
-const scmapping = mongoose.model('StudentCourseMapping',studentcoursemappingschema)
+const scmapping = mongoose.model('StudentCourseMapping', studentcoursemappingschema);
 
 function generateRandomId() {
     return Math.floor(Math.random() * 90000) + 10000;
 }
 
-module.exports = scmapping
+module.exports = scmapping;
